95144e5f18d07fe8759fc0bdb29bac0445326901
MauricioAcosta <andres.taamap@gmail.com>
2018-08-26 23:08:18 -0500

    Se realiza script en bash para ejecutar en segundo plano el app con cada contenedor

--- ejecutar_dockerfiles.sh
+++ ejecutar_dockerfiles.sh
@@ -3,13 +3,9 @@
 # Descargamos la imagen de postgres
 docker pull postgres:9.5 &
 docker run -rm postgres &
-
-
 # contruimos el contenedor de docker y lo ejecutamos
 docker build RESTful/ -t servidor &
 docker run --rm -ti -p 8080:8080 -u root servidor &
-
-
 # contruimos el contenedor de docker por el lado del cliete y corremos la prueba
 docker build cliente/ -t cliente &
 docker run -it --rm cliente &
